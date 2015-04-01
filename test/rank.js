66083719d708cf18991bb67f77989911080388d8
Raül Pérez <repejota@gmail.com>
2015-04-01 22:30:24 +0200

    Organized tests

--- test/rank.js
+++ test/rank.js
@@ -4,13 +4,13 @@ var rank = require("../index.js").rank;
 
 describe("Rank calculation", function() {
 
+    var points = 72;
+
+    var hours = 36;
+
+    var gravity = 1.8;
+
     describe("Calulate the rank for an element", function() {
-
-        var points = 72;
-
-        var hours = 36;
-
-        var gravity = 1.8;
 
         var score;
 
@@ -20,7 +20,7 @@ describe("Rank calculation", function() {
         });
 
         it('is bigger than 0', function(done) {
-            (score>0).should.be.true;
+            (score > 0).should.be.true;
             done();
         });
 
@@ -29,19 +29,31 @@ describe("Rank calculation", function() {
             done();
         });
 
+    });
+
+    describe("Rank variation factor based on published time", function() {
+
+        var score;
+
+        before(function(done) {
+            score = rank.rank(points, hours, gravity);
+            done();
+        });
+
         it('decreases rank if it is older', function(done) {
             var older_hours = hours + 1;
             var older_score = rank.rank(points, older_hours, gravity);
-            (score>older_score).should.be.true;
+            (score > older_score).should.be.true;
             done();
         });
 
         it('increases rank if it is fresh', function(done) {
             var earlier_hours = hours - 1;
             var earlier_score = rank.rank(points, earlier_hours, gravity);
-            (score<earlier_score).should.be.true;
+            (score < earlier_score).should.be.true;
             done();
         });
 
     });
+
 });
