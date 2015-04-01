6222983f6c1358200ecffc49f5f1bf6581019118
Raül Pérez <repejota@gmail.com>
2015-04-01 22:22:54 +0200

    Added rank calculation using hacker news algorithm

---
+++ test/rank.js
@@ -0,0 +1,47 @@
+var should = require("should");
+
+var rank = require("../index.js").rank;
+
+describe("Rank calculation", function() {
+
+    describe("Calulate the rank for an element", function() {
+
+        var points = 72;
+
+        var hours = 36;
+
+        var gravity = 1.8;
+
+        var score;
+
+        before(function(done) {
+            score = rank.rank(points, hours, gravity);
+            done();
+        });
+
+        it('is bigger than 0', function(done) {
+            (score>0).should.be.true;
+            done();
+        });
+
+        it('is correct', function(done) {
+            should.equal(0.10177658506910982, score);
+            done();
+        });
+
+        it('decreases rank if it is older', function(done) {
+            var older_hours = hours + 1;
+            var older_score = rank.rank(points, older_hours, gravity);
+            (score>older_score).should.be.true;
+            done();
+        });
+
+        it('increases rank if it is fresh', function(done) {
+            var earlier_hours = hours - 1;
+            var earlier_score = rank.rank(points, earlier_hours, gravity);
+            (score<earlier_score).should.be.true;
+            done();
+        });
+
+    });
+});
